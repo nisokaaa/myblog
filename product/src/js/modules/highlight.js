59b414c4d653910f5e5a132a6fa06a1654abff5f
nisokaaa <www.nisoka@gmail.com>
2022-04-23 20:06:45 +0900

    文字エスケープに関するバグ修正

--- product/src/js/modules/highlight.js
+++ product/src/js/modules/highlight.js
@@ -1,15 +1,27 @@
 import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
+import css from 'highlight.js/lib/languages/css';
+import php from 'highlight.js/lib/languages/php';
+import scss from 'highlight.js/lib/languages/scss';
+import xml from 'highlight.js/lib/languages/xml';
+import bash from 'highlight.js/lib/languages/bash';
+
 import 'highlight.js/styles/github-dark.css'; //CSSスタイル変更はここ
 
 hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('css', css);
+hljs.registerLanguage('php', php);
+hljs.registerLanguage('scss', scss);
+hljs.registerLanguage('xml', xml);
+hljs.registerLanguage('bash', bash);
 
 document.addEventListener("DOMContentLoaded", function() {
   const highlightBlocks = document.getElementsByClassName('code__body');
 
   for (let i = 0; i < highlightBlocks.length; i++) {
     // ハイライト後のコードを取得し、既存コードと入れ替え
-    const highlightedCode = hljs.highlightAuto(highlightBlocks[i].innerHTML).value;
+    const highlightedCode = hljs.highlightAuto(highlightBlocks[i].innerText).value;
     highlightBlocks[i].innerHTML = highlightedCode;
   }
+  
 }, {passive: true});
--- product/theme/template-parts/acf-blocks/code.php
+++ product/theme/template-parts/acf-blocks/code.php
@@ -1,12 +1,21 @@
 <?php
 /*--------------------------------------------------------------
 >>> [AFC block] 「コード」ブロック
---------------------------------------------------------------*/ ?>
+--------------------------------------------------------------*/
+// エスケープ処理を行う。
+// 
+// 特殊文字が含まれた文字列をHTMLエンティティに変換する。
+// 「<script></script>」→「&lt;script&gt;&lt;/script&gt;」にしてエラーを回避する。
+//
+// 特殊文字とは「 , ' " & < > 」のこと。
+// HTMLエンティティとは「アンパサンド & で始まりセミコロン ; で終わるテキスト」のこと。
+?>
 <?php 
 $body = get_field('code-body');
+$escaped_code = htmlspecialchars($body, ENT_QUOTES, 'UTF-8');
 ?>
 <div class="code">
   <div class="code__inner">
-    <pre><code class="code__body"><?php echo $body; ?></code></pre>
+    <pre><code class="code__body"><?php echo $escaped_code; ?></code></pre>
   </div>
 </div>
